3dac3e24c35938b78f8ede0c660c4db046edc8ac
Emmakin <101441304+Emmakin@users.noreply.github.com>
2023-05-01 13:57:29 +0100

    Started working on the body

--- Dropdown navigation/src/components/Body/body.jsx
+++ Dropdown navigation/src/components/Body/body.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import BackP from "../images/image-hero-desktop.png"
+import "./body.css"
 
 function Body() {
   return (
-    <div>body</div>
+    <div className='wrapperB'>
+        <div><img src={BackP}/></div>
+        <div className='bodyW'>
+            <div className='mainT'>
+                <h1>
+                    <p>Make</p>
+                    <p>remote work</p>
+                </h1>
+            </div>
+            <div className='bodyT'>
+                <p>Get your team in sync, no matter your location</p>
+                <p>Streamline processes, create tean rituals, and</p>
+                <p>watch productivity soar.</p>
+            </div>
+            <button>Learn more</button>
+        </div>
+    </div>
   )
 }
 
